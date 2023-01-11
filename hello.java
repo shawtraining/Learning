f3a51c20783639187a3f9dd2eb3a0e9add49a5b3
Babalu kumar <kumarbabalu957@gmail.com>
2023-01-11 21:28:12 +0530

    hello.java arr

--- hello.java
+++ hello.java
@@ -2,5 +2,10 @@ import java.util.*;
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int[] arr=new int[6];
+        for(int i=0;i<5;i++){
+            arr[i]=1;
+            System.out.print(arr[i]+" ");
+        }
     }
 }
